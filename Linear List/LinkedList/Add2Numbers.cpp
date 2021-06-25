9274f0d63af75582460c0ae1190fa7fe090aea65
jianfei2 <jianfei2@ualberta.ca>
2021-06-25 15:20:16 -0600

    6.25

---
+++ Linear List/LinkedList/Add2Numbers.cpp
@@ -0,0 +1,34 @@
+/**
+ * 
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(-1);
+        int carry = 0;
+        ListNode *prev = &dummy;
+        for(ListNode *pa = l1, *pb = l2;
+            pa != nullptr ||pb != nullptr;
+            pa = pa ==nullptr ? nullptr : pa->next,
+            pb = pb ==nullptr ? nullptr : pb->next,
+            prev = prev->next){
+            
+            const int ai = pa == nullptr ? 0 : pa->val;
+            const int bi = pb == nullptr ? 0 : pb->val;
+            const int value = (ai + bi + carry) % 10;
+            carry = (ai + bi + carry) / 10;
+            prev->next = new ListNode(value); // insert new node!!!
+        }
+        if(carry > 0)
+            prev->next = new ListNode(carry);
+        return dummy.next;
+    }
+};
